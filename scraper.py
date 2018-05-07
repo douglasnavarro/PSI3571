04817d7870c1987777f7014e8126645b5bb9b187
Douglas <douglasnavarro94@gmail.com>
2018-05-06 22:43:44 -0300

    prepared for continuous run

--- scraper.py
+++ scraper.py
@@ -1,16 +1,20 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import time
+import logging
 
-try:
-    vq_home = requests.get('http://www.viaquatro.com.br/').text
-except:
-    vq_home = None
+lines_metro = ['azul', 'verde', 'vermelha', 'amarela', 'lilas', 'prata']
+lines_cptm  = ['rubi', 'diamante', 'esmeralda', 'turquesa', 'coral', 'safira']
 
-if(vq_home is not None):
-    s = BeautifulSoup(vq_home, 'html.parser')
-else:
-    s = None
+
+file_handler = logging.FileHandler('log.txt', mode='a')
+logger = logging.getLogger(__name__)
+file_formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)-4s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logging.basicConfig(level=logging.DEBUG)
+logger.info('Starting scraper')
 
 def get_operation_status(soup):
 
@@ -29,7 +33,10 @@ def get_operation_status(soup):
     'prata': ''
     }
 
-    status['amarela'] = soup.find('img', id="imageCurrentLineFourStatus")['alt']
+    status_amarela = soup.find('img', id="imageCurrentLineFourStatus")['alt']
+    if(status_amarela == 'Operação Normal'):
+        status_amarela = 'normal'
+    status['amarela'] = status_amarela
     # if('normal' in status_amarela):
     #     status_amarela = 'normal'
     # elif('reduzida' in status_amarela):
@@ -43,7 +50,9 @@ def get_operation_status(soup):
         if(len(name_and_status) == 2):
             name = name_and_status[0].text.lower()
             station_status = name_and_status[1].text.lower()
-            status[name] = station_status    
+            status[name] = station_status
+    status['lilas'] = status['lilás']
+    del status['lilás']
     return(status)
 
 def get_time_data(soup):
@@ -62,6 +71,35 @@ def get_time_data(soup):
     metro = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}',metro).group()
     cptm = re.search(r'\d{2}/\d{2}/\d{4} \d{2}:\d{2}',cptm).group()
         
-    return(line4, metro, cptm)
+    return {'line4':line4, 'metro':metro, 'cptm':cptm}
 
-print(get_operation_status(s))
+while(True):
+
+    try:
+        vq_home = requests.get('http://www.viaquatro.com.br/')
+        if(vq_home.status_code == 200):
+            vq_home = vq_home.text
+        else:
+            vq_home = None
+            continue
+    except:
+        vq_home = None
+        continue
+
+    
+    s = BeautifulSoup(vq_home, 'html.parser')
+    times = get_time_data(s)
+    op_status = get_operation_status(s)
+
+    with open('data.txt', 'a') as d:
+        for line in lines_metro:
+            if(line == 'amarela'):
+                d.write('{},{},{}\n'.format(times['line4'],line, op_status[line]))
+            else:
+                d.write('{},{},{}\n'.format(times['metro'], line, op_status[line]))
+        for line in lines_cptm:
+            d.write('{},{},{}\n'.format(times['cptm'],line, op_status[line]))
+    
+    logger.info('Sleeping for 120 seconds')
+    time.sleep(120)
+    
